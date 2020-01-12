7d8b420f6c4394052604a41512b06ba77a56e275
Phillip Youn <phillipyoun@gmail.com>
2020-01-11 17:30:27 -0800

    Place code in right section
    Had placed all code in the fixtures.js section by mistake. Needed to
    place those changes in the specified stringifyJSON.js file.
    
    Also removed changes to fixture.js

--- spec/fixtures.js
+++ spec/fixtures.js
@@ -35,39 +35,6 @@ unstringifiableValues = [
   }
 ];
 
-function stringifyJSON(obj){
-//early returns
-  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null){
-    return ''+ obj;
-    //why the double quotation here when there is no space? 
-  // checkstrings
-  }else if (typeof obj === 'string'){
-    return '"' + obj + '"';
-  //check array
-  }else if (Array.isArray(obj)){
-    var results = [];
-    if (obj.length === 0){
-      return '[' + results + ']';
-    }
-    obj.forEach(function(item){
-      results.push(stringifyJSON(item));
-    })
-    return '[' + results + ']';
-  //check object
-  }else{
-    var results = [];
-    for (var key in obj){
-      if (obj[key] === undefined || typeof obj[key] === 'function'){
-        continue;
-      }
-       results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
-    }
-   return '{' + results + '}';
-  }
-  
-  
-}
-
 parseableStrings = [
   // basic stuff
   '[]',
