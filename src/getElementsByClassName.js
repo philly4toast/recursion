13d784ce2e26ba4837f9dc4bae13eacb35abbe55
Phillip Youn <phillipyoun@gmail.com>
2020-01-11 20:04:32 -0800

    Complete getElementsByClassName
    Both of the bare minimum requirements have been completed.
    
    Study/review and take an attempt at advanced content.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+// your code here
+	var results = [];
+	function searchElementForClassName(element){
+			//if some element contains className and exists
+		if (element.classList && element.classList.contains(className)){
+			//add it to the results
+			results.push(element);
+		}
+			//add recursively to chec
+			//if that element has child nodes and the elements within there contain className
+		if (element.childNodes){
+			element.childNodes.forEach(function(item){
+				searchElementForClassName(item);
+			})
+		}
+	}	
+			//call upon our document.body
+	searchElementForClassName(document.body);	
+			//return results
+	return results;
+  
 };
